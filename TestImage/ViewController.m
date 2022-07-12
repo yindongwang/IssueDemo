48b77de351df6b4cd559025870fb7e96ce2c4ba6
王寅冬 <wangyd@lenovocloud.com>
2022-07-12 17:22:49 +0800

    pod latest SDWebImage

--- TestImage/ViewController.m
+++ TestImage/ViewController.m
@@ -6,8 +6,11 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+MultiFormat.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UIImageView *imageView;
 
 @end
 
@@ -16,7 +19,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"test" style:UIBarButtonItemStylePlain target:self action:@selector(loadImage:)];
+    self.navigationItem.rightBarButtonItem = item;
+    
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 100, 400, 400)];
+    [self.view addSubview:self.imageView];
 }
 
+- (void)loadImage:(id)sender {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"77778be556396754169c81b2930f5c9d857" ofType:@"ai"];
+    NSData *data = [[NSData alloc] initWithContentsOfFile:path];
+    UIImage *image = [UIImage sd_imageWithData:data];
+    self.imageView.image = image;
+}
 
 @end
